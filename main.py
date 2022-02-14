44c05d5d6d9766700e680f738cd7cd3c0fbd6d7f
Resten <saup19@naver.com>
2022-02-14 15:31:36 +0900

    Main

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+import pyproj
+from urllib.parse import urlparse
+
+searchEnglishAddress = "https://www.juso.go.kr/addrlink/addrEngApi.do"
+searchEnglishAddressKey = "devU01TX0FVVEgyMDIyMDIxNDE1MDUzNTExMjI0MjA="
+
+searchPositionAddress = "https://www.juso.go.kr/addrlink/addrCoordApi.do"
+searchPositionAddressKey = "devU01TX0FVVEgyMDIyMDIxNDE1MDYzNjExMjI0MjE"
+
+def main():
+    print("A")
+
+
+
+if __name__ == '__main__':
+    main()
