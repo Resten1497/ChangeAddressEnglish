e5442902292297173161bc9c65cf955e304d1679
Resten <saup19@naver.com>
2022-03-03 18:26:32 +0900

    update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import urllib.parse
 
 import requests
-import pyproj
+from pyproj import Proj, Transformer
 import json
 from urllib import parse
 
@@ -11,35 +11,33 @@ searchEnglishAddressParam = {"confmKey": searchEnglishAddressKey,
                              "keyword": "",
                              "resultType": "json"
                              }
-
 searchPositionAddress = "https://www.juso.go.kr/addrlink/addrCoordApi.do"
 searchPositionAddressKey = "devU01TX0FVVEgyMDIyMDIxNDE1MDYzNjExMjI0MjE="
 searchPositionAddressParam = {
-    "confmKey": searchPositionAddressKey,
-    "resultType": "json",
     "admCd": "",
     "rnMgtSn": "",
     "udrtYn": "",
     "buldMnnm": "",
     "buldSlno": "",
-
 }
 
 
 def main():
-    enAddressResult = requestEnAddress(setEnAddress())
-    print(requestEnAddress(setPositionAddress(enAddressResult)))
+    address = input()
+    enAddressResult = requestAPI(setEnAddress(address))
+    positionResult = requestAPI(setPositionAddress(enAddressResult))
+    if positionResult is not None :
+        print(positionResult['results']['juso'][0]['entX'], positionResult['results']['juso'][0]['entY'])
+        print(project_array())
 
-
-def setEnAddress():
-    searchEnglishAddressParam['keyword'] = '강원 춘천시 공지로 353'
+def setEnAddress(address):
+    searchEnglishAddressParam['keyword'] = address
     parameter = urllib.parse.urlencode(searchEnglishAddressParam)
     # query = parse.parse_qs(url.query)
     return searchEnglishAddress + "?" + parameter
 
 
-def requestEnAddress(url):
-    print(url)
+def requestAPI(url):
     res = requests.get(url)
     json_object = json.loads(res.text)
     return json_object
@@ -47,14 +45,26 @@ def requestEnAddress(url):
 
 def setPositionAddress(item):
     #print(item['results']['juso'][0]['admCd'])
-    for i in searchPositionAddressParam:
-        if i != "confmKey" and i != "resultType":
+    if len(item['results']['juso']) > 1:
+        for i in searchPositionAddressParam:
             searchPositionAddressParam[i] = item['results']['juso'][0][i]
-    parameter = urllib.parse.urlencode(searchPositionAddressParam)
-    return searchPositionAddress + "?" + parameter
+        searchPositionAddressParam['confmKey'] = searchPositionAddressKey
+        searchPositionAddressParam['resultType'] = 'json'
+        print(searchPositionAddressParam)
+
+        parameter = urllib.parse.urlencode(searchPositionAddressParam)
+
+        return searchPositionAddress + "?" + parameter
+
+    return None
 
 
+def project_array( ):
 
+    transformer = Transformer.from_crs("epsg:5179","epsg:4326")
+    result = transformer.transform('1985581.651174346', '1019968.41192052')
+
+    print(result[0],result[1])
 
 
 if __name__ == '__main__':
